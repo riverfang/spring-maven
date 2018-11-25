d0cc86e4e3a5b588283fb757402fdc40fa78dde6
windsnail <2847780985@qq.com>
2018-11-25 20:34:09 +0800

    init

---
+++ learn-spring/learn-spring-web/src/main/java/cn/com/riverfang/test/Demo.java
@@ -0,0 +1,12 @@
+package cn.com.riverfang.test;
+
+/**
+*@description
+*@author  fangjiang
+*@date 2018/10/4 19:14
+*/
+public class Demo {
+    public void test1 () {
+
+    }
+}
---
+++ learn-spring/learn-spring-web/src/main/java/cn/com/riverfang/test/Demo2.java
@@ -0,0 +1,9 @@
+package cn.com.riverfang.test;
+
+/**
+ * @author fangjiang
+ * @description
+ * @date 2018/10/4
+ */
+public class Demo2 {
+}
